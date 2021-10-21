37b3047cc8b8783c75b9ed7b4850dd245992863f
Cominelli98 <a.cominelli009@studenti.unibs.it>
2021-10-21 15:34:52 +0200

    aggiunto jUnit jupiter

---
+++ src/it/unibs/ingesw/TestA.java
@@ -0,0 +1,14 @@
+package it.unibs.ingesw;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestA {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
