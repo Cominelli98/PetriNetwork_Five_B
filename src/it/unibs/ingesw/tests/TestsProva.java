62e61b7f027e41ff7dba66c3c4a2605b88f91d1f
Cominelli98 <a.cominelli009@studenti.unibs.it>
2021-10-27 11:54:59 +0200

    aggiunto package utility

--- src/it/unibs/ingesw/tests/TestsProva.java
+++ src/it/unibs/ingesw/tests/TestsProva.java
@@ -1,12 +1,19 @@
 package it.unibs.ingesw.tests;
 
 import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 import it.unibs.ingesw.*;
 
 public class TestsProva {
 
 	@Test
 	public void pacipaci() {
+		
+		var asd = 12;
+		var dsa = 12;
+		assertTrue(asd == dsa);
+//		assertFalse(true);
 	}
 	
 }
