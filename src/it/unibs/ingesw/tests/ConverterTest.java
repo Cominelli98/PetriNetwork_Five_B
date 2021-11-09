eb04a125699e24e8e5311a0acb3872908d2b0b5c
Cominelli98 <a.cominelli009@studenti.unibs.it>
2021-11-09 17:19:34 +0100

    nouvo javaProject tests

--- src/it/unibs/ingesw/tests/ConverterTest.java
+++ src/it/unibs/ingesw/tests/ConverterTest.java
@@ -1,5 +1,31 @@
 package it.unibs.ingesw.tests;
 
+import org.junit.*;
+import java.util.ArrayList;
+import it.unibs.ingesw.*;
+
 public class ConverterTest {
 
+	@Before
+	public void setUp() {
+		
+		Network net1 = new Network("Net");
+		net1.addTransition("tran");
+		net1.addLocation("loc1");
+		net1.addLocation("loc2");
+		Link l1 = new Link(net1.getLocation(0).getId(), net1.getLastTransition().getId(), net1.getId(), 1);
+		Link l2 = new Link(net1.getLastTransition().getId(), net1.getLocation(1).getId(), net1.getId(), 1);
+		net1.addLink(l1);
+		net1.addLink(l2);
+		
+		Petri_network pnet = new Petri_network(net1, "PNet");
+//		Petri_transition PTran = new Petri_transition(netId, nodeId, nodeName);
+		ArrayList<Petri_transition> trans = new ArrayList<Petri_transition> ();
+		
+	}
+	
+	@Test
+	public void toPetriTest() {
+		
+	}
 }
